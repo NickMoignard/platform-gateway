d64a6ed3da0a3fb3f991e73d707dd87dcd3d07d0
Nick Moignard <nicklmoignard@gmail.com>
2023-01-26 19:20:30 +1100

    added authz fixed tsconfig

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseFilters, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpExceptionFilter, JwtAuthGuard } from 'authz';
 
+@UseFilters(HttpExceptionFilter)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthzModule } from 'authz';
 
 @Module({
-  imports: [],
+  imports: [AuthzModule],
   controllers: [AppController],
   providers: [AppService],
 })
